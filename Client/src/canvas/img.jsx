247a9bb528a1df7f77601397ef8d0b7f0f73002e
shubham rathore <sr1849587@gmail.com>
2024-10-21 19:56:17 +0530

    photo

--- Client/src/canvas/img.jsx
+++ Client/src/canvas/img.jsx
@@ -2,7 +2,7 @@ import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { PerspectiveCamera, OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import { Model } from "../../Scene";
-import musasi from "../assets/chill.png"
+import musasi from "../assets/luffy.jpg"
 
 const House = () => {
   return (
