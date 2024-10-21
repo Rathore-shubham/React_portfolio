432365dfb595e157fa764362be8fdb604895e949
shubham rathore <sr1849587@gmail.com>
2024-10-21 20:37:54 +0530

    firse photo update

--- Client/src/canvas/img.jsx
+++ Client/src/canvas/img.jsx
@@ -1,15 +1,24 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera, OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import {
+  PerspectiveCamera,
+  OrbitControls,
+  Preload,
+  useGLTF,
+} from "@react-three/drei";
 import { Model } from "../../Scene";
-import musasi from "../assets/ye.jpg"
+import musasi from "../assets/ye.jpg";
 
 const House = () => {
   return (
-    <>
-        <img src={musasi} alt="musasi" className="w-full h-auto"/>
-    </>
-  )
-}
+    <div class="lg:h-[80vh] md:h-[60vh] sm:h-[50vh] h-[40vh] w-full flex items-center justify-center">
+      <img
+        src={musasi}
+        alt="musasi"
+        className="w-full h-auto max-h-[80vh] max-w-[70%]"
+      />
+    </div>
+  );
+};
 
-export default House
+export default House;
